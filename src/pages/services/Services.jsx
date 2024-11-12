73c2e5ffe213aa40aa328eee5579e05d48864957
alpha <godspowerlawrence008@gmail.com>
2024-11-12 05:26:39 -0500

    services page

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,13 +1,97 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import './service.css'
 
 const Services = () => {
   return (
-    <section className="pt-20 flex flex-col items-center justify-center px-8 " >
-        <div className="text-6xl">page not found</div>
-         <p className="mt-4 text-lg">Sorry, the product you are looking for does not exist.</p>
-         <Link to="/" className="mt-6 text-blue-500 underline">
-          Go back to Home
-       </Link>
+    <section className="pt-28 " >
+        <div className="mx-[15%] " style={{color:'azure'}}>
+            <h1 className="my-3 text-center py-5 text-default text-2xl">BENEFITS OF VIRTUAL CARD</h1>
+            <div className="md:flex bg-default py-24">
+              <div className="flex-col md:w-1/2  lg:px-[10%] md:px-[10%]  px-[12%]">
+                <h4 className="pb-4 pt-5" >EASE OF CANCELLATION</h4>
+                <p className="text-sm">If Virtual Card is compromised it can be easily cancelled</p>
+                <p className="text-sm pb-3">And Replaced without Affecting The User's Primary Account</p>
+              </div>
+              <div className="flex-col md:w-1/2">Image div</div>
+            </div>
+        </div>
+        <div className="mx-[15%] mt-10 shadow ">
+            <div className="md:flex text-default py-24">
+            <div className="flex-col md:w-1/2">Image div</div>
+              <div className="flex-col md:w-1/2  lg:px-[10%] md:px-[10%]  px-[12%]">
+                <h4 className="pb-4 pt-5" >ENHANCE SECURITY</h4>
+                <p className="text-sm">Virtual Card Reduce The Risk Of Fraud By Generating A</p>
+                <p className="text-sm ">Unique Card Number For Each Transaction</p>
+                <p>Which Can Help Prevent Unauthorised Use.</p>
+              </div>
+
+            </div>
+        </div>
+        <div className="mx-[15%] mt-10 " style={{color:'azure'}}>
+            <div className="md:flex bg-default py-24">
+              <div className="flex-col md:w-1/2  lg:px-[10%] md:px-[10%]  px-[12%]">
+                <h4 className="pb-4 pt-5" >FLEXIBILITY</h4>
+                <p className="text-sm">If Virtual Card is compromised it can be easily cancelled</p>
+                <p className="text-sm pb-3">And Replaced without Affecting The User's Primary Account</p>
+              </div>
+              <div className="flex-col md:w-1/2">Image div</div>
+            </div>
+        </div>
+        <div className="mx-[15%] mt-10 shadow ">
+            <div className="md:flex text-default py-24">
+            <div className="flex-col md:w-1/2">Image div</div>
+              <div className="flex-col md:w-1/2  lg:px-[10%] md:px-[10%]  px-[12%]">
+                <h4 className="pb-4 pt-5" >IMPROVE ACCOUNTABILITY</h4>
+                <p className="text-sm">Virtual Card refers To The Increased Transparency And Control These Cards offer In Financial Transactions </p>
+              </div>
+
+            </div>
+        </div>
+        <div className="mx-[15%] mt-10 " style={{color:'azure'}}>
+            <div className="md:flex bg-default py-24">
+              <div className="flex-col md:w-1/2  lg:px-[10%] md:px-[10%]  px-[12%]">
+                <h4 className="pb-4 pt-5" >FRAUD PROTECTION</h4>
+                <p className="text-sm">Fraud protection Is A Key faeature Of Virtual Cards, Designed To Enhance Security FOr Online Transactions</p>
+
+              </div>
+              <div className="flex-col md:w-1/2">Image div</div>
+            </div>
+        </div>
+        <div className="mx-[15%] mt-10 shadow ">
+            <div className="md:flex text-default py-24">
+            <div className="flex-col md:w-1/2">Image div</div>
+              <div className="flex-col md:w-1/2  lg:px-[10%] md:px-[10%]  px-[12%]">
+                <h4 className="pb-4 pt-5" >GLOBAL ACCEPTANCE</h4>
+                <p className="text-sm"> Virtual Cards Are Typically Accepted Anywhere That Accepts Major Credit Cards, Providing Users With FLexibility In International Transactions.</p>
+                <p></p>
+              </div>
+
+            </div>
+        </div>
+        <div className="mx-[15%] mt-10 " style={{color:'azure'}}>
+            <div className="md:flex bg-default py-24">
+              <div className="flex-col md:w-1/2  lg:px-[10%] md:px-[10%]  px-[12%]">
+                <h4 className="pb-4 pt-5" >EASE OF ONLINE PURCHANCE</h4>
+                <p className="text-sm">Users Can Set Up Virtual Cards For Subscription Services, Helping To Mange Recurring Payments Withount Exposing Primsry Card Information</p>
+                <p className="text-sm pb-3">And Replaced without Affecting The User's Primary Account</p>
+              </div>
+              <div className="flex-col md:w-1/2">Image div</div>
+            </div>
+        </div>
+        <div className="mx-[15%] mt-10 mb-20 shadow  ">
+            <div className="md:flex text-default py-24">
+            <div className="flex-col md:w-1/2">Image div</div>
+              <div className="flex-col md:w-1/2  lg:px-[10%] md:px-[10%]  px-[12%]">
+                <h4 className="pb-4 pt-5" >BUDGETING TOOLS</h4>
+                <p className="text-sm">Many virtual Card Services Offer Budegting And Trading Users Manage Their Finances More Effectively</p>
+                <p></p>
+              </div>
+
+            </div>
+        </div>
+        
+        
+        
     </section>
   )
 }
